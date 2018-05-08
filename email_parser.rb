c6019a9c11114de70d5949e4941e7425ee0260fe
Ashley <a.bloskas@yahoo.com>
2018-05-07 19:19:37 -0700

    first commit

---
+++ email_parser.rb
@@ -0,0 +1,36 @@
+def parser
+    start_number=0
+    arr = Array.new
+    @boundary_starting = false
+    text=File.open('email.txt').read
+    text.gsub!(/\r\n?/, "\n")
+    text.each_line do |line|
+        # puts "#{start_number += 1} #{line}"     #was used for visual clarity
+        # puts line.split(/: */)[1]               #was used for visual clarity
+        arr << line if (line["To:"])
+        arr << line if (line["From:"])
+        arr << line if (line["Subject:"])
+        if (line["boundary="])
+            @boundary = "--"+line[/boundary="(.*?)"/m, 1]
+            puts @boundary
+            puts "--------------"
+        end
+        if line["#{@boundary}"]
+            if @boundary_starting == false
+            @boundary_starting = true
+            else
+                @boundary_starting = false
+            end
+        else
+            if @boundary_starting == true
+                arr << line
+            end    
+        end
+    end 
+    puts "*********************"
+    puts "*********************"
+    puts "*********************"    
+    puts arr    
+end
+parser()
+
